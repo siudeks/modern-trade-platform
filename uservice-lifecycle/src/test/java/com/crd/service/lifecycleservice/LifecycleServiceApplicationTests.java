1924550b706c5785fdb98629457a5ba92dfce70f
Fearghal McShane <fearghalmcshane@gmail.com>
2022-10-20 15:21:56 +0100

    Add lifecycle service

---
+++ uservice-lifecycle/src/test/java/com/crd/service/lifecycleservice/LifecycleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crd.service.lifecycleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifecycleServiceApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
